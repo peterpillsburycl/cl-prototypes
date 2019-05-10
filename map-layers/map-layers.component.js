b65b9406c8803881ef80637b8a9877db1cfe1fc3
jkulunk-cl <jkulunk@corelogic.com>
2019-05-10 09:25:24 -0700

    toggle without jquery

--- map-layers/map-layers.component.js
+++ map-layers/map-layers.component.js
@@ -1,14 +1,32 @@
-$(function() {
-    
+$(function () {
+
     // Tooltip
     $(".j-layerListItems .NavBar_menuItem.disabled").tooltip({
         "title": "Zoom-in to enable"
     });
 
-    // Toggle Date Range Options
-    $(".mls_status_option").change(function() {
+
+    /*
+
+    // Toggle Date Range Options with jQuery
+
+     $(".mls_status_option").change(function() {
         var target = this.getAttribute('data-target');
         $('#' + target).toggle();
     });
 
+    */ 
+
+
+    // Toggle Date Range Options without jQuery
+    var mlsStatusOptions = document.querySelectorAll('.mls_status_option');
+
+    mlsStatusOptions.forEach(function (el) {
+        el.addEventListener('change', function () {
+            var dataTarget = this.getAttribute('data-target');
+            document.getElementById(dataTarget).classList.toggle('show');
+        });
+    });
+
+    
 });
